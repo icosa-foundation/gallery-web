a1cc2713ae8ee315c8fbb0fa3b18dfbee77938ea
Luna Winters <luna@gocreate.fun>
2021-02-25 13:23:33 +0000

    Home Hero component now generates Hero Slider using API stubs

--- src/components/home-hero/homehero.js
+++ src/components/home-hero/homehero.js
@@ -1,15 +1,71 @@
 import React from "react"
-import { Container, Row, Col, Button } from "react-bootstrap"
+import { Container, Row, Col } from "react-bootstrap"
+import HeroSlider, { Slide, Nav, OverlayContainer } from "hero-slider"
 import "./homehero.scss"
 
 const HomeHero = (props) => {
+  const { content } = props
+  console.log("Change Autoplay to on")
   return (
     <div className="home-hero">
-      <Container>
-        <Row>
-          <Col>Home Hero</Col>
-        </Row>
-      </Container>
+      <HeroSlider
+        orientation="horizontal"
+        initialSlide={1}
+        onBeforeChange={(previousSlide, nextSlide) =>
+          console.log("onBeforeChange", previousSlide, nextSlide)
+        }
+        onChange={(nextSlide) => console.log("onChange", nextSlide)}
+        onAfterChange={(nextSlide) => console.log("onAfterChange", nextSlide)}
+        style={{
+          backgroundColor: "#000",
+        }}
+        settings={{
+          slidingDuration: 500,
+          slidingDelay: 100,
+          shouldAutoplay: true,
+          shouldDisplayButtons: false,
+          autoplayDuration: 5000,
+          height: "30vh",
+        }}
+      >
+        <OverlayContainer className="overlay">
+          <Container>
+            <Row>
+              <Col>
+                <h2>A Virtual Reality Art Portal</h2>
+              </Col>
+            </Row>
+          </Container>
+        </OverlayContainer>
+
+        {content.map((content, key) => {
+          return (
+            <Slide
+              navDescription={content.title}
+              background={{
+                backgroundImage: content.image_url,
+                backgroundAnimation: "zoom",
+              }}
+              className="slide"
+            >
+              <Container>
+                <Row>
+                  <Col>
+                    <div className="projectinfo">
+                      <div className="title">{content.title}</div>
+                      <div>
+                        <div className="author">{content.author}</div>|
+                        <div className="date">{content.datetime}</div>
+                      </div>
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+            </Slide>
+          )
+        })}
+        <Nav />
+      </HeroSlider>
     </div>
   )
 }
--- src/components/home-hero/homeherocontroller.js
+++ src/components/home-hero/homeherocontroller.js
@@ -1,9 +1,24 @@
 import React from "react"
 import HomeHero from "./homehero"
+import ProjectsAPI from "../../api/projects"
 
 class Controller extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { content: [] }
+  }
+
+  componentDidMount() {
+    this.getContent()
+  }
+
   render() {
-    return <HomeHero />
+    return <HomeHero content={this.state.content} />
+  }
+
+  async getContent() {
+    const projects = await ProjectsAPI.getProjectList("featured", 3, 0)
+    this.setState({ content: projects })
   }
 }
 export default Controller
