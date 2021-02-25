0e01273c6050b778b5d2051b94228b7d3d20d19a
Luna Winters <luna@gocreate.fun>
2021-02-25 10:22:53 +0000

    Code style cleanup on home hero controller

--- src/components/home-hero/homeherocontroller.js
+++ src/components/home-hero/homeherocontroller.js
@@ -1,11 +1,9 @@
-import React from 'react'
-import HomeHero from './homehero'
+import React from "react"
+import HomeHero from "./homehero"
 
 class Controller extends React.Component {
-
   render() {
     return <HomeHero />
   }
-  
 }
 export default Controller
