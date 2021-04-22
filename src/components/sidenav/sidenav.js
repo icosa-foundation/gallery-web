3e7ad0eca2a24f3d301a3aac824452ea8de68914
Luna Winters <luna@gocreate.fun>
2021-04-22 09:16:19 +0100

    Added Layout for Sidenav

--- src/components/sidenav/sidenav.js
+++ src/components/sidenav/sidenav.js
@@ -1,9 +1,19 @@
 import React from "react"
-import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarContent, SidebarFooter } from "react-pro-sidebar"
+import { Link } from "react-router-dom"
+import { ProSidebar, Menu, MenuItem, SidebarHeader, SidebarContent, SidebarFooter, SubMenu } from "react-pro-sidebar"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faBars, faTimes, faCoffee } from "@fortawesome/free-solid-svg-icons"
+import { faBars, faTimes, faHome, faTh, faUser, faHeart, faCube } from "@fortawesome/free-solid-svg-icons"
 import "react-pro-sidebar/dist/scss/styles.scss"
 import "./sidenav.scss"
+import obLogo from "./oblogo.jpg"
+
+const Showable = (props) => {
+  if (props.collapsed || !props.children) {
+    return null
+  } else {
+    return props.children
+  }
+}
 
 const SideNav = (props) => {
   const { collapsed, toggleNav } = props
@@ -13,33 +23,69 @@ const SideNav = (props) => {
         <div className="toggler">
           <FontAwesomeIcon className="toggleicon" icon={collapsed ? faBars : faTimes} onClick={toggleNav} />
         </div>
-        {!collapsed ? (
+        <Showable {...props}>
           <div className="text-center">
             <h2>Icosa Gallery</h2>{" "}
           </div>
-        ) : (
-          ""
-        )}
+        </Showable>
       </SidebarHeader>
       <SidebarContent>
         <Menu iconShape="square">
-          <MenuItem icon={<FontAwesomeIcon icon={faCoffee} />}>Dashboard</MenuItem>
-          <SubMenu title="Components">
-            <MenuItem>Component 1</MenuItem>
-            <MenuItem>Component 2</MenuItem>
-          </SubMenu>
+          <MenuItem icon={<FontAwesomeIcon icon={faHome} />}>
+            Home
+            <Link to="/" />
+          </MenuItem>
+          <MenuItem icon={<FontAwesomeIcon icon={faTh} />}>
+            Dashboard
+            <Link to="/" />
+          </MenuItem>
+        </Menu>
+        <Menu iconShape="square">
+          <Showable {...props}>
+            <h3>Me</h3>
+          </Showable>
+          <MenuItem icon={<FontAwesomeIcon icon={faUser} />}>
+            My Profile
+            <Link to="/" />
+          </MenuItem>
+          <MenuItem icon={<FontAwesomeIcon icon={faHeart} />}>
+            My Likes
+            <Link to="/" />
+          </MenuItem>
+        </Menu>
+        <Menu iconShape="square">
+          <Showable {...props}>
+            <h3>Explore</h3>
+          </Showable>
+          <MenuItem icon={<FontAwesomeIcon icon={faCube} />}>
+            Poly Legacy
+            <Link to="/" />
+          </MenuItem>
+          <MenuItem icon={<FontAwesomeIcon icon={faBars} />}>
+            Animals
+            <Link to="/" />
+          </MenuItem>
+          <MenuItem icon={<FontAwesomeIcon icon={faBars} />}>
+            History
+            <Link to="/" />
+          </MenuItem>
+          <MenuItem icon={<FontAwesomeIcon icon={faBars} />}>
+            Science
+            <Link to="/" />
+          </MenuItem>
         </Menu>
       </SidebarContent>
       <SidebarFooter>
-        {!collapsed ? (
+        <Showable {...props}>
           <div className="text-center">
-            <a href="https://github.com/icosa-gallery/" target="_blank">
+            <a href="https://openbrush.org" rel="noreferrer" target="_blank" alt="OpenBrush">
+              <img src={obLogo} />
+            </a>
+            <a href="https://github.com/icosa-gallery/" rel="noreferrer" target="_blank">
               Support us on Github!
             </a>
           </div>
-        ) : (
-          ""
-        )}
+        </Showable>
       </SidebarFooter>
     </ProSidebar>
   )
