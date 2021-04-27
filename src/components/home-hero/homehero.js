897305cb868588de6f24008b798531848a38cbd7
Luna Winters <luna@gocreate.fun>
2021-04-27 17:52:44 +0100

    Removed Hero slider component and replaced with bootstrap equivalent

--- src/components/home-hero/homehero.js
+++ src/components/home-hero/homehero.js
@@ -1,6 +1,5 @@
 import React from "react"
-import { Container, Row, Col } from "react-bootstrap"
-import HeroSlider, { Slide, Nav, OverlayContainer } from "hero-slider"
+import { Container, Row, Col, Carousel } from "react-bootstrap"
 import moment from "moment"
 import "./homehero.scss"
 
@@ -8,60 +7,21 @@ const HomeHero = (props) => {
   const { content } = props
   return (
     <div className="home-hero">
-      <HeroSlider
-        orientation="horizontal"
-        initialSlide={1}
-        style={{
-          backgroundColor: "#000",
-        }}
-        settings={{
-          slidingDuration: 500,
-          slidingDelay: 100,
-          shouldAutoplay: true,
-          shouldDisplayButtons: false,
-          autoplayDuration: 5000,
-          height: "",
-        }}
-      >
-        <OverlayContainer className="overlay">
-          <Container>
-            <Row>
-              <Col>
-                <h2>A Virtual Reality Art Portal</h2>
-              </Col>
-            </Row>
-          </Container>
-        </OverlayContainer>
-
+      <Carousel>
         {content.map((content, key) => {
           return (
-            <Slide
-              key={key}
-              navDescription={content.title}
-              background={{
-                backgroundImage: content.thumbnail.url,
-                backgroundAnimation: "zoom",
-              }}
-              className="slide"
-            >
-              <Container>
-                <Row>
-                  <Col>
-                    <div className="projectinfo">
-                      <div className="title">{content.displayName}</div>
-                      <div>
-                        <div className="author">{content.authorName}</div>|
-                        <div className="date">{moment(content.createTime).format("YYYY-MM-DD")}</div>
-                      </div>
-                    </div>
-                  </Col>
-                </Row>
-              </Container>
-            </Slide>
+            <Carousel.Item>
+              <img className="d" src={content.thumbnail.url} alt={content.displayName + " by " + content.authorName} />
+              <Carousel.Caption>
+                <h3 className="title">{content.displayName}</h3>
+                <p>
+                  {content.authorName} | {moment(content.createTime).format("YYYY-MM-DD")}
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
           )
         })}
-        <Nav />
-      </HeroSlider>
+      </Carousel>
     </div>
   )
 }
