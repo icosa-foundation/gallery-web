feafd0ee73f0b185ef953543dc8e15ab08f94646
Luna Winters <luna@gocreate.fun>
2021-04-27 19:17:03 +0100

    Link to Poly page for Featured Sketches

--- src/components/home-hero/homehero.js
+++ src/components/home-hero/homehero.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Container, Row, Col, Carousel } from "react-bootstrap"
+import { Link } from "react-router-dom"
 import moment from "moment"
 import "./homehero.scss"
 
@@ -10,10 +11,13 @@ const HomeHero = (props) => {
       <Carousel>
         {content.map((content, key) => {
           return (
-            <Carousel.Item>
+            <Carousel.Item key={key}>
               <img className="d" src={content.thumbnail.url} alt={content.displayName + " by " + content.authorName} />
               <Carousel.Caption>
-                <h3 className="title">{content.displayName}</h3>
+                {console.log(content)}
+                <Link to={"/poly/view/" + content.name.replace("assets/", "")}>
+                  <h3 className="title">{content.displayName}</h3>
+                </Link>
                 <p>
                   {content.authorName} | {moment(content.createTime).format("YYYY-MM-DD")}
                 </p>
