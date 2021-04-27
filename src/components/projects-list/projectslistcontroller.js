857bf51510586c09daf2293ca353372c6b072293
Luna Winters <luna@gocreate.fun>
2021-04-27 20:05:42 +0100

    Added Pagination to Asset view, requires API to be updated

--- src/components/projects-list/projectslistcontroller.js
+++ src/components/projects-list/projectslistcontroller.js
@@ -6,21 +6,48 @@ import PolyProjectsAPI from "../../api/poly/projects"
 class Controller extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { content: [] }
+    this.state = { content: [], page: 0 }
   }
 
   componentDidMount() {
     this.getContent()
+    window.addEventListener("scroll", this.handleScroll, false)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll, false)
+  }
+
+  handleScroll = (e) => {
+    const bottom = Math.ceil(window.innerHeight + window.scrollY) >= document.documentElement.scrollHeight
+    if (bottom) {
+      this.setState({ page: this.state.page + 1 }, () => {
+        this.getContent()
+      })
+    }
   }
 
   async getContent() {
+    let projects = []
     if (this.props.isPoly) {
-      const projects = await PolyProjectsAPI.getProjectList("full", 25, 0)
-      this.setState({ content: projects })
+      projects = await PolyProjectsAPI.getProjectList("full", 25, this.state.page)
     } else {
-      const projects = await ProjectsAPI.getProjectList("full", 25, 0)
-      this.setState({ content: projects })
+      projects = await ProjectsAPI.getProjectList("full", 25, this.state.page)
     }
+    const content = this.state.content
+    for (const p of projects) {
+      let isUnique = true
+      for (const c of content) {
+        if (c.name === p.name) {
+          isUnique = false
+          break
+        }
+      }
+      if (isUnique) {
+        content.push(p)
+      }
+    }
+    this.setState({ content })
   }
 
   render() {
