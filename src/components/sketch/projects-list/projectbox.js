0a5b8a955c0045e35ab628173b70c93cc8b42e6d
Luna Winters <luna@gocreate.fun>
2021-04-29 18:45:17 +0100

    Use poly logo if no thumbnail is found

--- src/components/sketch/projects-list/projectbox.js
+++ src/components/sketch/projects-list/projectbox.js
@@ -1,16 +1,18 @@
 import React from "react"
 import moment from "moment"
+import noasset from "./noasset.png"
 
 const ProjectBox = (props) => {
   const { project, isPoly } = props
   const projectURL = (isPoly ? "/poly" : "") + "/view/" + project.name.replace("assets/", "")
+  const thumbnail = project.thumbnail.url ? `url(${project.thumbnail.url})` : `url(${noasset})`
   return (
     <div className="projectbox">
       <a href={projectURL}>
         <div
           className="projectimage"
           style={{
-            backgroundImage: `url(${project.thumbnail.url})`,
+            backgroundImage: thumbnail,
           }}
         >
           <div className="title">
