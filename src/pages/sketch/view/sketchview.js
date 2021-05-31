9a4921432ef94d7a010a1aa003113ac5f2a38699
Luna Winters <luna@gocreate.fun>
2021-05-31 18:09:38 +0100

    Remove unused import on SketchView

--- src/pages/sketch/view/sketchview.js
+++ src/pages/sketch/view/sketchview.js
@@ -1,5 +1,4 @@
 import { withRouter } from "react-router"
-import AssetsAPI from "../../../api/assets"
 import SketchInfo from "../../../components/sketch/sketch-info"
 import SketchViewer from "../../../components/sketch/sketch-viewer"
 import "./sketchview.scss"
