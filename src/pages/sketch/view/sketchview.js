197d9ad5944f3e339b4a2324471a21d25c1bc6ea
Mike Nisbet <nisbet.mr@gmail.com>
2021-07-13 23:56:50 +0100

    Move viewer logic to web

--- src/pages/sketch/view/sketchview.js
+++ src/pages/sketch/view/sketchview.js
@@ -1,17 +1,35 @@
+import React from "react"
 import { withRouter } from "react-router"
 import SketchInfo from "../../../components/sketch/sketch-info"
 import SketchViewer from "../../../components/sketch/sketch-viewer"
 import "./sketchview.scss"
+import Loader from "../../../components/ui/loader"
+import AssetsAPI from "../../../api/assets"
 
-function SketchView(props) {
-  const { id, userid } = props.match.params
+class SketchView extends React.Component {
+  constructor(props) {
+    const { id, userid } = props.match.params
+    super(props)
+    this.state = { loading: true, id, userid }
+    this.getAsset()
+  }
 
-  return (
-    <div>
-      <SketchViewer id={id} userid={userid} />
-      <SketchInfo id={id} userid={userid} />
-    </div>
-  )
+  getAsset = async () => {
+    const asset = await AssetsAPI.getAsset(this.state.userid, this.state.id)
+    this.setState({ asset: asset, loading: false })
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <Loader />
+    }
+    return (
+      <div>
+        <SketchViewer asset={this.state.asset} />
+        <SketchInfo id={this.state.id} userid={this.state.userid} />
+      </div>
+    )
+  }
 }
 
 export default withRouter(SketchView)
