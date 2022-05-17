dfba549816b16bcce40885232f4bd72162218eed
Leo Blondel <leo@blondel.ninja>
2022-05-17 14:59:37 +0200

    editing for review

--- src/pages/sketch/embed/sketchviewembed.js
+++ src/pages/sketch/embed/sketchviewembed.js
@@ -10,29 +10,10 @@ class SketchViewEmbed extends React.Component {
   constructor(props) {
     const { id, userid } = props.match.params
     super(props)
-    const params = this.parseQuery(props.location.search)
+    const params = new URLSearchParams(props.location.search)
     this.state = { loading: true, id, userid, params }
     this.getAsset()
   }
-
-  parseQuery = (queryString) => {
-  var query = {};
-  var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
-  for (var i = 0; i < pairs.length; i++) {
-    var pair = pairs[i].split('=');
-    query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
-    if (Number(query[decodeURIComponent(pair[0])])) {
-      query[decodeURIComponent(pair[0])] = Number(query[decodeURIComponent(pair[0])])
-    }
-    if (query[decodeURIComponent(pair[0])] == "true") {
-      query[decodeURIComponent(pair[0])] = true
-    }
-    if (query[decodeURIComponent(pair[0])] == "false") {
-      query[decodeURIComponent(pair[0])] = false
-    }
-  }
-  return query;
-}
 
   getAsset = async () => {
     const asset = await AssetsAPI.getAsset(this.state.userid, this.state.id)
@@ -46,12 +27,10 @@ class SketchViewEmbed extends React.Component {
     return (
       <div className="sketchviewer-embedded" style={{width: "100%", height: "100%"}}>
         <SketchViewer asset={this.state.asset} embedded={true}/>
-        {this.state.params.footer === true ? 
+        {this.state.params.has('footer') == true && 
           <div className="embedded-footer">
             <SketchInfo id={this.state.id} userid={this.state.userid} />
           </div>
-          :
-          null
         }
       </div>
     )
